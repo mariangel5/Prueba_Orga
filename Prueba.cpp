79a8e7d9a8362bdfcca78f80467287f9eaa3e066
mariangel5 <mariangel.avendano.ramirez@est.una.ac.cr>
2023-11-13 10:43:02 -0600

    calculadora

--- Prueba.cpp
+++ Prueba.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+
 using namespace std;
 
-int main(){
+double obtenerNumero() {
+    double numero;
+    cout << "Ingrese un número: ";
+    cin >> numero;
+    return numero;
+}
 
-	cout << "Esto es una prueba" << endl;
+char obtenerOperador() {
+    char operador;
+    cout << "Ingrese un operador (+, -, *, /): ";
+    cin >> operador;
+    return operador;
+}
 
-	return 0;
+int main() {
+    char operador;
+    double numero1, numero2;
+
+    cout << "Calculadora Simple" << endl;
+    cout << "------------------" << endl;
+
+    operador = obtenerOperador();
+    numero1 = obtenerNumero();
+    numero2 = obtenerNumero();
+
+    switch (operador) {
+    case '+':
+        cout << "Resultado: " << numero1 + numero2 << endl;
+        break;
+    case '-':
+        cout << "Resultado: " << numero1 - numero2 << endl;
+        break;
+    case '*':
+        cout << "Resultado: " << numero1 * numero2 << endl;
+        break;
+    case '/':
+        if (numero2 != 0) {
+            cout << "Resultado: " << numero1 / numero2 << endl;
+        }
+        else {
+            cout << "Error: División por cero no permitida." << endl;
+        }
+        break;
+    default:
+        cout << "Operador no válido. Por favor, ingrese un operador válido." << endl;
+        break;
+    }
+
+    return 0;
 }
